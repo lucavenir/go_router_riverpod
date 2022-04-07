c0e0e0c25506f4b23a4493c3faa6d084bdaab7e9
Luca Venir <luca@venir.dev>
2022-04-08 01:05:59 +0200

    Imported the example from the main repo

---
+++ android/app/src/main/kotlin/com/example/go_router/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.go_router
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/go_router_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.go_router_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
