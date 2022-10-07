a79164e9d6ea467f6c1a59b7b7c23f82d3afbab0
venir.dev <luca@venir.dev>
2022-10-07 10:57:16 +0200

    clean up

--- legacy/value_notifier/test/widget_test.dart
+++ legacy/value_notifier/test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:value_notifier/main.dart';
