f0ac3e3c2c61669d5240867b73bed9d330c91be5
venir.dev <luca@venir.dev>
2022-10-07 10:56:05 +0200

    Added the sync example in its stand alone folder

---
+++ sync_router/test/widget_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sync_router/main.dart';
+
+void main() {
+  testWidgets('Our first frame shows the home', (tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const ProviderScope(child: SyncRouterApp()));
+
+    // The initial location for our router is `/`,
+    // but the redirect is immediately triggered
+    expect(find.text("Login Page"), findsOneWidget);
+    expect(find.text('Home Page'), findsNothing);
+
+    // Tap the login button
+    await tester.tap(find.bySemanticsLabel('Login'));
+    await tester.pumpAndSettle(const Duration(milliseconds: 750));
+
+    // // Verify that we're back to the login page
+    expect(find.text("Login Page"), findsNothing);
+    expect(find.text('Home Page'), findsOneWidget);
+  });
+}
